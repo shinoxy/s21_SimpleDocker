c0dbbc856d7c4d22d26eca91752b2a4b18692b3e
Viktoria Shchepeleva <102042269+shinoxy@users.noreply.github.com>
2023-07-13 00:13:18 +0300

    Add files via upload

--- server/server.c
+++ server/server.c
@@ -1,19 +1,15 @@
-s#include <fcgi_stdio.h>
+#include <fcgi_stdio.h>
 
-void handle_request()
-{
+void handle_request() {
     printf("Content-type: text/html\r\n\r\n");
     printf("<html><head><title>Hello World</title></head><body>");
     printf("<h1>Hello World!</h1>");
     printf("</body></html>");
 }
 
-int main()
-{
-    while (FCGI_Accept() >= 0)  // Непрерывная обработка запросов, пока FastCGI-сервер не будет остановлен или произойдет ошибка.
-    {
+int main() {
+    while (FCGI_Accept() >= 0) {
         handle_request();
     }
-
     return 0;
 }
